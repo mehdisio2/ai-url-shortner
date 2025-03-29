a3b5b6d99d2745f5dcee0b16d364dae72e712447
EL MAHDI BADILOU <mehdibadilou7@gmail.com>
2025-03-29 09:41:34 +0000

    initialize Node.js project with Express server and essential middleware

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const routes = require("./routes"); // Importing routes
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON request body
+app.use(cors());         // Enable CORS
+
+
+// Routes
+app.use("/api", routes);
+
+module.exports = app;
