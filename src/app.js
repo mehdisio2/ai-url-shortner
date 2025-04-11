edbf25a6f7e716fa921a51801b5b123aff560516
Mahdi Badilou <mehdibadilou7@gmail.com>
2025-04-11 08:45:39 +0100

    add PostgreSQL integration and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const routes = require("./routes"); // Importing routes
-
+const routes = require("./routes/routes"); // Importing routes
 const app = express();
+
+const { Client } = require('pg');
+require('dotenv').config({ path: '/home/mahdi/Documents/Projects/ai-url-shortner/.env' });  // To load the variables from the .env file
+
+
+// Create a new client instance
+const client = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+// Connect to the PostgreSQL database
+client.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL!');
+  })
+  .catch((err) => {
+    console.error('Connection error', err.stack);
+  });
+
 
 // Middleware
 app.use(express.json()); // Parse JSON request body
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,15 +1,18 @@
 
 //controller to shorten a URL 
 exports.urlShortner = (req, res) => {
+    console.log('urlRedirectToOriginal called')
+    
+
 
 }
 
 //controller to redirect to the original URL
 exports.urlRedirectToOriginal = (req, res) => {
-
+    console.log('urlRedirectToOriginal called')
 }
 
 //controller to delete a sshorten URL
 exports.deleteShortenUrl = (req, res) => {
-
+    console.log('deleteShortenUrl called')
 }
